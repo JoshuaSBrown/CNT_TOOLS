7531d8df4f8965891fed6f6707735848bd67e597
JoshuaSBrown <joshbro42867@yahoo.com>
2018-08-21 22:58:51 -0600

    Cleaned house

--- CNT_Rotation_Angle.m
+++
@@ -1,8 +0,0 @@
-function [ theta ] = CNT_Rotation_Angle( n, m )
-%The rotation angle describes the relationship between the two
-%Coordinate systems (kx,ky) and (kT, kC)
-
-theta = abs(atan(-(n-m)/((3)^(1/2)*(n+m))));
-
-end
-
